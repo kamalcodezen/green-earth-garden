b767e38c47cfe87796fa5dadaa2f8debe2a4ac67
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-10 16:21:52 +0530

    all plants show and categor button api access

--- javaScript/script.js
+++ javaScript/script.js
@@ -8,14 +8,25 @@ const treesContainer = document.getElementById("treesContainer");
 
 
 
-// all categories api
+// all categories api button
 const allCategoriesBtnApi = async () => {
-
     const res = await fetch("https://openapi.programming-hero.com/api/categories")
     const data = await res.json()
     displayCategories(data.categories)
 
 }
+
+// all button categorie api card 
+const btnCategoryApi = async (id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/category/${id}`)
+    const data = await res.json()
+    console.log(data.plants)
+
+}
+
+
+
+// sob button gulo dispaly koralam
 const displayCategories = (categories) => {
 
     categoriesContainer.innerHTML = "";
@@ -25,22 +36,21 @@ const displayCategories = (categories) => {
         const button = document.createElement("button")
         button.className = "btn btn-primary w-full"
         button.innerText = categorie.category_name;
-
+        button.onclick = () => btnCategoryApi(categorie.id)
         categoriesContainer.append(button)
 
     });
 
 }
 
-// all Trees card Api 
-
+// all Trees card Api sob card display koralam
 const allTreesBtnApi = async () => {
 
     const res = await fetch("https://openapi.programming-hero.com/api/plants");
     const data = await res.json()
     displayAllPlants(data.plants)
 }
-
+// all plants display
 const displayAllPlants = (trees) => {
 
     treesContainer.innerHTML = "";
@@ -50,27 +60,25 @@ const displayAllPlants = (trees) => {
         const div = document.createElement("div");
         div.className = `card p-3 bg-white shadow-sm border-b-2 ${tree.price > 500 ? "border-red-500" : "border-green-500"} w-full w-[350px]`;
         div.innerHTML = `
-<div class="card bg-white shadow-sm h-full">
-    <figure>
-        <img src="${tree.image}" class="w-full h-[180px] object-cover mb-1"/>
-    </figure>
-
-    <div class=" flex flex-col w-full space-y-1">
-        <h2 class="card-title">${tree.name}</h2>
-
-        <p class="line-clamp-2">${tree.description}</p>
-
-        
-
-        <div class="flex justify-between items-center mt-auto">
-        <div class="badge badge-success badge-outline">${tree.category}</div>
-            <h2 class="font-bold text-xl text-[#4ade80]">$${tree.price}</h2>
-            
-        </div> 
-        <button class="btn btn-primary rounded-full hover:opacity-90">Buy Now</button>
-    </div>
-</div>
-`;
+                              <div class="card bg-white shadow-sm h-full">
+                                  <figure>
+                                      <img src="${tree.image}" class="w-full h-[180px] object-cover mb-1"/>
+                                  </figure>
+                              
+                                  <div class=" flex flex-col w-full space-y-1">
+                                      <h2 class="card-title">${tree.name}</h2>
+                              
+                                      <p class="line-clamp-2">${tree.description}</p>
+                              
+                                      <div class="flex justify-between items-center mt-auto">
+                                      <div class="badge badge-success badge-outline">${tree.category}</div>
+                                          <h2 class="font-bold text-xl text-[#4ade80]">$${tree.price}</h2>
+                                          
+                                      </div> 
+                                      <button class="btn btn-primary rounded-full hover:opacity-90">Buy Now</button>
+                                  </div>
+                              </div>
+                              `;
 
         treesContainer.append(div);
     });
@@ -80,12 +88,9 @@ const displayAllPlants = (trees) => {
 
 
 
-allTreesBtnApi('allTreesbtn');
-
-
-
 
 
 
 
 allCategoriesBtnApi();
+allTreesBtnApi('allTreesbtn');
