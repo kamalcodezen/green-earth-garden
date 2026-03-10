347f694a56b4563c96c413c569731bd9712a3904
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-10 15:57:37 +0530

    all api  trees show display

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,8 +1,14 @@
 
 const categoriesContainer = document.getElementById("categoriesContainer");
+const treesContainer = document.getElementById("treesContainer");
 
 
 
+
+
+
+
+// all categories api
 const allCategoriesBtnApi = async () => {
 
     const res = await fetch("https://openapi.programming-hero.com/api/categories")
@@ -10,13 +16,13 @@ const allCategoriesBtnApi = async () => {
     displayCategories(data.categories)
 
 }
-
 const displayCategories = (categories) => {
+
+    categoriesContainer.innerHTML = "";
 
     categories.forEach(categorie => {
 
         const button = document.createElement("button")
-
         button.className = "btn btn-primary w-full"
         button.innerText = categorie.category_name;
 
@@ -24,8 +30,62 @@ const displayCategories = (categories) => {
 
     });
 
+}
+
+// all Trees card Api 
+
+const allTreesBtnApi = async () => {
+
+    const res = await fetch("https://openapi.programming-hero.com/api/plants");
+    const data = await res.json()
+    displayAllPlants(data.plants)
+}
+
+const displayAllPlants = (trees) => {
+
+    treesContainer.innerHTML = "";
+
+    trees.forEach((tree) => {
+
+        const div = document.createElement("div");
+        div.className = `card p-3 bg-white shadow-sm border-b-2 ${tree.price > 500 ? "border-red-500" : "border-green-500"} w-full w-[350px]`;
+        div.innerHTML = `
+<div class="card bg-white shadow-sm h-full">
+    <figure>
+        <img src="${tree.image}" class="w-full h-[180px] object-cover mb-1"/>
+    </figure>
+
+    <div class=" flex flex-col w-full space-y-1">
+        <h2 class="card-title">${tree.name}</h2>
+
+        <p class="line-clamp-2">${tree.description}</p>
+
+        
+
+        <div class="flex justify-between items-center mt-auto">
+        <div class="badge badge-success badge-outline">${tree.category}</div>
+            <h2 class="font-bold text-xl text-[#4ade80]">$${tree.price}</h2>
+            
+        </div> 
+        <button class="btn btn-primary rounded-full hover:opacity-90">Buy Now</button>
+    </div>
+</div>
+`;
+
+        treesContainer.append(div);
+    });
+
 
 }
 
 
-allCategoriesBtnApi()
+
+allTreesBtnApi('allTreesbtn');
+
+
+
+
+
+
+
+allCategoriesBtnApi();
