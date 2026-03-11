30ea84c378aca2835b2b654995247c081660511a
kamalcodezen <kamaluddin7908@gmail.com>
2026-03-11 13:42:00 +0530

    spinner loading add and car design change

--- javaScript/script.js
+++ javaScript/script.js
@@ -5,8 +5,8 @@ const treesContainer = document.getElementById("treesContainer");
 const allTreesbtn = document.getElementById("allTreesbtn")
 const cartContainer = document.getElementById("cartContainer");
 const emptyCartMessage = document.getElementById("emptyCartMessage")
-
-
+const loadingSpinner = document.getElementById("treesLoadingSpinner");
+const totalPrice = document.getElementById("totalPrice")
 let cart = [];
 
 
@@ -28,7 +28,6 @@ const removeActiveBtn = (id) => {
     activeBtn.forEach(btn => btn.classList.remove("btn-primary"));
     allTreesbtn.classList.remove("btn-primary");
 
-
     const selectBtn = document.getElementById(id)
     selectBtn.classList.add("btn-primary");
 
@@ -39,9 +38,10 @@ const removeActiveBtn = (id) => {
 
 // all button categorie api card 
 const btnCategoryApi = async (id) => {
+    spinnerLoading(true)
     const res = await fetch(`https://openapi.programming-hero.com/api/category/${id}`)
     const data = await res.json()
-
+    spinnerLoading(false)
     btnCategoryCardDisplay(data.plants)
 
 }
@@ -54,27 +54,51 @@ const btnCategoryCardDisplay = (trees) => {
     trees.forEach((tree) => {
 
         const div = document.createElement("div");
-        div.className = `card p-3 bg-white shadow-sm border-b-2 ${tree.price > 500 ? "border-red-500" : "border-green-500"} w-full w-[350px]`;
+        div.className = ` bg-white shadow-sm border-b-2 ${tree.price > 500 ? "border-red-500" : "border-green-500"} w-full w-[350px]`;
         div.innerHTML = `
-                              <div class="card bg-white shadow-sm h-full">
-                                  <figure>
-                                      <img src="${tree.image}" class="w-full h-[180px] object-cover mb-1"/>
-                                  </figure>
+                              <div class="bg-white p-3 rounded-xl shadow-sm  flex flex-col h-full">
                               
-                                  <div class=" flex flex-col w-full space-y-1">
-                                      <h2 class="card-title">${tree.name}</h2>
-                              
-                                      <p class="line-clamp-2">${tree.description}</p>
-                              
-                                      <div class="flex justify-between items-center mt-auto">
-                                      <div class="badge badge-success badge-outline">${tree.category}</div>
-                                          <h2 class="font-bold text-xl text-[#4ade80]">$${tree.price}</h2>
-                                          
-                                      </div> 
-                                      <button onclick="addToCart(${tree.id},'${tree.name}',${tree.price})" class="btn btn-primary rounded-full hover:opacity-90">Buy Now</button>
-                                  </div>
-                              </div>
-                              `;
+                               <figure class="h-[150px] overflow-hidden">
+                                   <img 
+                                   src="${tree.image}" 
+                                   class="w-full h-full object-cover hover:scale-110 transition duration-300"
+                                   />
+                               </figure>
+                            
+                                <!-- Title -->
+                                <h2 class="font-semibold text-lg">
+                                    ${tree.name}
+                                </h2>
+                            
+                                <!-- Description -->
+                                <p class="text-sm text-gray-500 line-clamp-2 mt-1">
+                                    ${tree.description}
+                                </p>
+                            
+                                <!-- Category + Price -->
+                                <div class="flex justify-between items-center mt-3">
+                            
+                                    <span class="text-xs bg-green-100 text-green-700 px-3 py-1 rounded-full">
+                                        ${tree.category}
+                                    </span>
+                            
+                                    <span class="font-semibold text-gray-700">
+                                        $${tree.price}
+                                    </span>
+                            
+                                </div>
+                            
+                                <!-- Button -->
+                                <button 
+                                onclick="addToCart(${tree.id},'${tree.name}',${tree.price})"
+                                class="mt-4 bg-green-700 hover:bg-green-800 text-white py-2 rounded-full w-full">
+                            
+                                Add to Cart
+                            
+                                </button>
+                            
+                            </div>
+                            `;
 
         treesContainer.append(div);
     });
@@ -95,13 +119,13 @@ const displayCategories = (categories) => {
 
         const button = document.createElement("button");
 
-        button.className = "btn active-btn w-full";
-        button.id = `${categorie.id}`;
+        button.className = "btn active-btn w-full hover:bg-green-800  hover:text-white";
+        button.id = `cat-${categorie.id}`;
         button.innerText = categorie.category_name;
 
         button.onclick = () => {
             btnCategoryApi(categorie.id);
-            removeActiveBtn(`${categorie.id}`);
+            removeActiveBtn(`cat-${categorie.id}`);
         };
 
         categoriesContainer.append(button);
@@ -112,9 +136,10 @@ const displayCategories = (categories) => {
 
 // all Trees card Api sob card display koralam
 const allTreesBtnApi = async () => {
-
+    spinnerLoading(true)
     const res = await fetch("https://openapi.programming-hero.com/api/plants");
     const data = await res.json()
+    spinnerLoading(false)
     displayAllPlants(data.plants)
 }
 
@@ -126,28 +151,51 @@ const displayAllPlants = (trees) => {
     trees.forEach((tree) => {
 
         const div = document.createElement("div");
-        div.className = `card p-3 bg-white shadow-sm border-b-2 ${tree.price > 500 ? "border-red-500" : "border-green-500"} w-full w-[350px]`;
+        div.className = `card  bg-white shadow-sm border-b-2 ${tree.price > 500 ? "border-red-500" : "border-green-500"} w-full w-[350px]`;
         div.innerHTML = `
-                              <div class="card bg-white shadow-sm h-full">
-                                  <figure>
-                                      <img src="${tree.image}" class="w-full h-[180px] object-cover mb-1"/>
-                                  </figure>
+                              <div class="bg-white p-3 rounded-xl shadow-sm  flex flex-col h-full">
                               
-                                  <div class=" flex flex-col w-full space-y-1">
-                                      <h2 class="card-title">${tree.name}</h2>
-                              
-                                      <p class="line-clamp-2">${tree.description}</p>
-                              
-                                      <div class="flex justify-between items-center mt-auto">
-                                      <div class="badge badge-success badge-outline">${tree.category}</div>
-                                          <h2 class="font-bold text-xl text-[#4ade80]">$${tree.price}</h2>
-                                          
-                                      </div> 
-                                      <button  class="btn btn-primary rounded-full hover:opacity-90" onclick="addToCart(${tree.id}, '${tree.name}', ${tree.price})" >Buy Now</button>
-                                  </div>
-                              </div>
-                              `;
-
+                               <figure class="h-[150px] overflow-hidden">
+                                   <img 
+                                   src="${tree.image}" 
+                                   class="w-full h-full object-cover hover:scale-110 transition duration-300"
+                                   />
+                               </figure>
+                            
+                                <!-- Title -->
+                                <h2 class="font-semibold text-lg">
+                                    ${tree.name}
+                                </h2>
+                            
+                                <!-- Description -->
+                                <p class="text-sm text-gray-500 line-clamp-2 mt-1">
+                                    ${tree.description}
+                                </p>
+                            
+                                <!-- Category + Price -->
+                                <div class="flex justify-between items-center mt-3">
+                            
+                                    <span class="text-xs bg-green-100 text-green-700 px-3 py-1 rounded-full">
+                                        ${tree.category}
+                                    </span>
+                            
+                                    <span class="font-semibold text-gray-700">
+                                        $${tree.price}
+                                    </span>
+                            
+                                </div>
+                            
+                                <!-- Button -->
+                                <button 
+                                onclick="addToCart(${tree.id},'${tree.name}',${tree.price})"
+                                class="mt-4 bg-green-700 hover:bg-green-800 text-white py-2 rounded-full w-full">
+                            
+                                Add to Cart
+                            
+                                </button>
+                            
+                            </div>
+                            `;
         treesContainer.append(div);
     });
 
@@ -157,37 +205,51 @@ const displayAllPlants = (trees) => {
 // add cart section
 function addToCart(id, name, price) {
 
+    loadingSpinner.classList.remove("hidden");
+
     const upadateCartAll = cart.find((item) => item.id == id)
 
-    if (cart.length === 0) {
-        emptyCartMessage.classList.add("hidden")
-    }
+    setTimeout(() => {
 
-    if (upadateCartAll) {
-        upadateCartAll.quantity += 1
-    } else {
-        cart.push({
-            id,
-            name,
-            price,
-            quantity: 1
+        if (cart.length === 0) {
+            emptyCartMessage.classList.add("hidden")
+        }
 
-        })
+        if (upadateCartAll) {
+            upadateCartAll.quantity += 1
+        } else {
+            cart.push({
+                id,
+                name,
+                price,
+                quantity: 1
 
-    }
+            })
 
-    updateCart(cart);
+        }
+
+        updateCart(cart);
+
+        loadingSpinner.classList.add("hidden")
+
+    }, 300)
+
+
 }
 function updateCart(carts) {
 
     cartContainer.innerHTML = "";
 
+
+    let total = 0;
     for (let item of carts) {
+
+        total = total + item.price * item.quantity;
 
         const div = document.createElement("div")
         div.innerHTML = `
         
-                       <div class="card card-body bg-slate-100">
+                       <div class="bg-white p-3 rounded-xl shadow-sm  flex flex-col h-full">
                             <div class="flex justify-between items-center">
                                 <div>
                                     <h2>${item.name}</h2>
@@ -199,9 +261,11 @@ function updateCart(carts) {
                         </div>
         
         `;
+
         cartContainer.append(div);
 
     }
+    totalPrice.innerText = total;
 
 }
 
@@ -215,7 +279,16 @@ function removeCart(id) {
 }
 
 
+function spinnerLoading(status) {
+    if (status === true) {
+        loadingSpinner.classList.remove("hidden");
+        treesContainer.classList.add("hidden");
+    } else {
+        loadingSpinner.classList.add("hidden");
+        treesContainer.classList.remove("hidden");
+    }
 
+}
 
 
 
